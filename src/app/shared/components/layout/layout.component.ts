fc88b61f2728336f8bb5b969d10b6e241950c2f4
Kseniya Lipovskaya <lipovskayakseniya62@gmail.com>
2025-04-15 16:46:59 +0700

    fixed location of content

--- src/app/shared/components/layout/layout.component.ts
+++ src/app/shared/components/layout/layout.component.ts
@@ -1,15 +1,20 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from "../sidebar/sidebar.component";
+import { HeaderComponent } from "../header/header.component";
 
 @Component({
   selector: 'app-layout',
   imports: [
     RouterOutlet,
-    SidebarComponent
+    SidebarComponent,
+    HeaderComponent
 ],
   templateUrl: './layout.component.html',
-  styleUrl: './layout.component.scss'
+  styleUrl: './layout.component.scss',
+  host: {
+    '[class.collapsed]': 'isSidebarCollapsed()' 
+  }
 })
 export class LayoutComponent {
   isSidebarCollapsed = signal(false);
